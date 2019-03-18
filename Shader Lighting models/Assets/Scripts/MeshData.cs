e68282c5e52020d158ca02369ffe62cfd74f7301
João Pedro Pereira <joao.pedro.pereira@gmail.com>
2019-03-18 10:48:35 +0000

    [NEW] New Scene with colouring by vertex and fragment shader to understand them better

---
+++ Shader Lighting models/Assets/Scripts/MeshData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        foreach(Vector3 v in vertices) 
+        {
+            Debug.Log(v);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
